e84661b9d2877fc6051f5ddf3f5e9db9e1f526c3
Khillasaurus <D.A.Schenker@gmail.com>
2015-07-23 08:01:56 -0600

    Finished going through all files in the "Display" filter.
    Same objective as previous commit, simply typo correction, formatting
    and QUESTION comment addition.

--- Art Institute of Vancouver/Semester_8/VGP240 3D Graphics & Applications/mine/CMGfx/Rasterizer.cpp
+++ Art Institute of Vancouver/Semester_8/VGP240 3D Graphics & Applications/mine/CMGfx/Rasterizer.cpp
@@ -1,3 +1,7 @@
+//=============================================================================
+//Dependencies
+//=============================================================================
+
 #include "StdAfx.h"
 #include <gl/gl.h>
 #include <gl/glu.h>
@@ -5,43 +9,64 @@
 #include "MiniMath.h"
 #include "Rasterizer.h"
 
+//=============================================================================
+//Static Initialization
+//=============================================================================
+
 CRasterizer* CRasterizer::pinstance = 0;// initialize pointer
-CRasterizer* CRasterizer::Instance () 
+
+//=============================================================================
+//Class Definitions
+//=============================================================================
+
+CRasterizer* CRasterizer::Instance()
 {
-	if (pinstance == 0)  // is it the first call?
-	{  
-		pinstance = new CRasterizer; // create sole instance
+	//if no instance of this class has already been created
+	if(pinstance == 0)
+	{
+		//create sole instance (Singleton)
+		pinstance = new CRasterizer;
 	}
-	return pinstance; // address of sole instance
+	return pinstance;
 }
 
-void CRasterizer::SetColor( const float r, const float g, const float b )
+//-----------------------------------------------------------------------------
+
+void CRasterizer::SetColor(const float r, const float g, const float b)
 {
-	// Set OpenGL color
-	glColor3f( r, g, b );
+	//set OpenGL color
+	glColor3f(r, g, b);
 }
 
-void CRasterizer::DrawPoint( const int x, const int y )
+//-----------------------------------------------------------------------------
+
+void CRasterizer::DrawPoint(const int x, const int y)
 {
-	// Draw OpenGL pixel
-	glBegin( GL_POINTS );
-	glVertex2i( x, y );
+	//Draw OpenGL pixel
+	glBegin(GL_POINTS);
+		glVertex2i(x, y);
 	glEnd();
 }
 
-void CRasterizer::DrawPoint( const float x, const float y )
+//-----------------------------------------------------------------------------
+
+void CRasterizer::DrawPoint(const float x, const float y)
 {
-	// Draw OpenGL pixel
-	glBegin( GL_POINTS );
-	glVertex2i( (int)(x + 0.5f), (int)(y + 0.5f) );
+	//Draw OpenGL pixel
+	glBegin(GL_POINTS);
+		glVertex2i(static_cast<int>(x + 0.5f), static_cast<int>(y + 0.5f));
 	glEnd();
 }
 
-void CRasterizer::DrawPoint( const float x, const float y, const float r, const float g, const float b )
+//-----------------------------------------------------------------------------
+
+void CRasterizer::DrawPoint(const float x, const float y, const float r, const float g, const float b)
 {
-	SetColor( r, g, b );
-	DrawPoint( x, y );
+	SetColor(r, g, b);
+	DrawPoint(x, y);
 }
+
+//-----------------------------------------------------------------------------
 
 void CRasterizer::DrawLine(const float x1, const float y1, const float x2, const float y2)
 {
@@ -50,4 +75,3 @@ void CRasterizer::DrawLine(const float x1, const float y1, const float x2, const
 		glVertex2f(x2, y2);
 	glEnd();
 }
-
--- Art Institute of Vancouver/Semester_8/VGP240 3D Graphics & Applications/mine/CMGfx/Rasterizer.h
+++ Art Institute of Vancouver/Semester_8/VGP240 3D Graphics & Applications/mine/CMGfx/Rasterizer.h
@@ -1,27 +1,40 @@
 #ifndef __RASTERIZER_H__
 #define __RASTERIZER_H__
 
+//=============================================================================
+//Dependencies
+//=============================================================================
+
 #include <vector>
+
+//=============================================================================
+//Class Declarations
+//=============================================================================
 
 class CRasterizer
 {
 public:
+	//Class Singleton
 	static CRasterizer* Instance();
 protected:
+	//Singleton Duplication Prevention
+	//TOIMPROVE: These do not prevent duplication in ALL cases, but the vast majority of them, all of which should fall under the scope of this project. Exeption example: Multithreading may allow for more than one of this class to be constructed.
 	CRasterizer() {}
 	CRasterizer(const CRasterizer&);
 	CRasterizer& operator= (const CRasterizer&);
 private:
+	//Class Singleton Pointer
 	static CRasterizer* pinstance;
 
 public:
+	//QUESTION: What does this do? I could not find an implementation anywhere.
 	void Initialize() {}
 
-	void SetColor( const float r, const float g, const float b );
-	void DrawPoint( const int x, const int y );
-	void DrawPoint( const float x, const float y );
-	void DrawPoint( const float x, const float y, const float r, const float g, const float b );
-	void DrawLine(const float x1, const float y1, const float x2, const float y2);
+	void SetColor	(const float r,  const float g, const float b);
+	void DrawPoint	(const int	 x,  const int   y);
+	void DrawPoint	(const float x,  const float y);
+	void DrawPoint	(const float x,  const float y,  const float r,  const float g, const float b);
+	void DrawLine	(const float x1, const float y1, const float x2, const float y2);
 };
 
-#endif // __RASTERIZER_H__
+#endif //#ifndef __RASTERIZER_H__
